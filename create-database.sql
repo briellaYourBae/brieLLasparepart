11fb47df90efdec9742ca42091fb88de3ed9da92
BrieLLaYourBae <briellayb@gmail.com>
2026-03-08 21:53:56 +0700

    Update database name to db_briellasparepart for MySQL

--- create-database.sql
+++ create-database.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS briellasparepart CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE briellasparepart;
+CREATE DATABASE IF NOT EXISTS db_briellasparepart CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE db_briellasparepart;
